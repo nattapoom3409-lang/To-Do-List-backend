90d4a191a31980df707ce2700670045ac8e84364
Nattapoom <nattapoom3409@gmail.com>
2025-10-08 19:32:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ const app = express();
 // ✅ Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://to-do-list-nattapoom.netlify.app", 
-    ],    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
@@ -21,10 +19,10 @@ app.use(cookieParser());
 
 // ✅ Database Connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "to-do-list",
 });
 
 // ✅ Register
@@ -261,6 +259,4 @@ app.get("/tasks/search/:listId", (req, res) => {
 });
 
 // ✅ Start server
-const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-
+app.listen(8081, () => console.log("✅ Server running on port 8081"));
